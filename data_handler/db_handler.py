935ef36453cf2aa7fa9a3d96de288c44210b9a8b
pratyush-velotio <pratyush.pranav@velotio.com>
2025-07-30 20:34:19 +0530

    Fixed sqlite datatype

--- data_handler/db_handler.py
+++ data_handler/db_handler.py
@@ -21,14 +21,14 @@ class DBHandler:
     def create_generated_data_table(self):
         self.conn.execute('''
             CREATE TABLE IF NOT EXISTS generated_data (
-                user_id Text,
-                full_name TEXT,
-                product_name TEXT,
-                category TEXT,
-                Price float,
-                address TEXT,
+                user_id VARCHAR2,
+                full_name VARCHAR2,
+                product_name VARCHAR2,
+                category VARCHAR2,
+                Price FLOAT,
+                address VARCHAR2,
                 available BOOLEAN DEFAULT 1,
-                created_at TEXT
+                created_at VARCHAR2
             );
         ''')
         self.conn.commit()
@@ -41,24 +41,24 @@ class DBHandler:
         self.conn.commit()
 
     def insert_generated_data(self, records: List[Dict]):
-        for record in records:
-            self.conn.execute("""
-                INSERT OR IGNORE INTO generated_data (
-                    user_id, full_name, product_name, category,
-                    Price, available, created_at
-                ) VALUES (?, ?, ?, ?, ?, ?, ?)
-            """, (
-                record.get("user_id"),
-                record.get("full_name"),
-                record.get("product_name"),
-                record.get("category"),
-                record.get("Price"),
-                record.get("address"),
-                record.get("available"),
-                record.get("created_at")
-            ))
-            break
-        self.conn.commit()
+        print(self.conn.execute("SELECT * FROM generated_data").fetchall())
+        # for record in records:
+        #     self.conn.execute("""
+        #         INSERT OR IGNORE INTO generated_data (
+        #             user_id, full_name, product_name, category,
+        #             Price, address, available, created_at
+        #         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        #     """, (
+        #         record.get("user_id"),
+        #         record.get("full_name"),
+        #         record.get("product_name"),
+        #         record.get("category"),
+        #         record.get("Price"),
+        #         str(record.get("address")),
+        #         record.get("available"),
+        #         record.get("created_at")
+        #     ))
+        # self.conn.commit()
         
     def get_metadata(self, file_id):
         cursor = self.conn.execute(
